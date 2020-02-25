65a88e9635f2a01834eb89e950a16da21559f406
lamsharkie <gibbs.brandon.d@gmail.com>
2020-02-25 10:59:35 -0800

    added path for searches/new.ejs and added a form to that page. Renders as expected.

--- server.js
+++ server.js
@@ -13,18 +13,31 @@ const client = new pg.Client(process.env.DATABASE_URL);
 
 const PORT = process.env.PORT || 3001;
 
+
+// Getting that EJS stuff on the page
 app.use(express.urlencoded({extended: true}));
 app.use(express.static('./public'));
 app.set('view engine', 'ejs');
+
+
+// Go home!
 app.get('/', (request, response) => {
     response.render('./pages/index.ejs');
 })
 
+// Just in case things go wrong.
 app.get('/test', (request, response) => {
     response.render('./pages/index.ejs');
     console.log('This is the test route calling. Something is not right. Pick up your phone!');
 })
 
+app.get('/searches/new', (request, response)=> {
+    response.render('./pages/searches/new.ejs')
+})
+
+
+
+// Turn this thing ONNNN!
 app.listen(PORT, () => {
     console.log(`Yo Yo Yo. Mike check one two, one two. Listening on ${PORT}`);
 })
